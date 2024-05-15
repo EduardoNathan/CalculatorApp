66f8d0345cbbfcb6f8f91901421f4540e85f23f4
Eduardo Nathan <eduardo.nathan2018@outlook.com>
2024-05-15 19:17:30 -0300

    adicionando a tela de historico, e adicionando binding.

--- app/src/main/java/com/kenaidev/calculatorapp/MainActivity.java
+++ app/src/main/java/com/kenaidev/calculatorapp/MainActivity.java
@@ -1,86 +1,76 @@
 package com.kenaidev.calculatorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.kenaidev.calculatorapp.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kenaidev.calculatorapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnPlus, btnMinus, btnDivision, btnTimes;
-    EditText edtFirstNumber, edtSecondNumber;
-    TextView txtResult;
-    Button btnOpen;
-
     int numeroUm, numeroDois;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        btnPlus = findViewById(R.id.btn_plus);
-        btnMinus = findViewById(R.id.btn_minus);
-        btnDivision = findViewById(R.id.btn_division);
-        btnTimes = findViewById(R.id.btn_times);
-        btnOpen = findViewById(R.id.btn_open);
+        binding.btnPlus.setOnClickListener(this);
+        binding.btnMinus.setOnClickListener(this);
+        binding.btnDivision.setOnClickListener(this);
+        binding.btnTimes.setOnClickListener(this);
 
-        edtFirstNumber = findViewById(R.id.edt_first_number);
-        edtSecondNumber = findViewById(R.id.edt_second_number);
+        binding.btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        txtResult = findViewById(R.id.txt_result);
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(intent);
 
-
-        btnPlus.setOnClickListener(this);
-        btnMinus.setOnClickListener(this);
-        btnDivision.setOnClickListener(this);
-        btnTimes.setOnClickListener(this);
-        btnOpen.setOnClickListener(this);
-
+            }
+        });
 
     }
 
     @Override
     public void onClick(View view) {
-        if (edtFirstNumber.getText().toString().isEmpty() || edtSecondNumber.getText().toString().isEmpty()) {
+        if (binding.edtFirstNumber.getText().toString().isEmpty() || binding.edtSecondNumber.getText().toString().isEmpty()) {
             Toast.makeText(this, "Preencha os campos.", Toast.LENGTH_SHORT).show();
         } else if (view.getId() == R.id.btn_plus) {
 
-            numeroUm = Integer.parseInt(edtFirstNumber.getText().toString());
-            numeroDois = Integer.parseInt(edtSecondNumber.getText().toString());
+            numeroUm = Integer.parseInt(binding.edtFirstNumber.getText().toString());
+            numeroDois = Integer.parseInt(binding.edtSecondNumber.getText().toString());
 
             int resultado = numeroUm + numeroDois;
 
-            txtResult.setText(String.valueOf(resultado));
+            binding.txtResult.setText(String.valueOf(resultado));
 
         } else if (view.getId() == R.id.btn_minus) {
 
-            numeroUm = Integer.parseInt(edtFirstNumber.getText().toString());
-            numeroDois = Integer.parseInt(edtSecondNumber.getText().toString());
+            numeroUm = Integer.parseInt(binding.edtFirstNumber.getText().toString());
+            numeroDois = Integer.parseInt(binding.edtSecondNumber.getText().toString());
 
             int resultado = numeroUm - numeroDois;
 
-            txtResult.setText(String.valueOf(resultado));
+            binding.txtResult.setText(String.valueOf(resultado));
 
         } else if (view.getId() == R.id.btn_division) {
 
-            numeroUm = Integer.parseInt(edtFirstNumber.getText().toString());
-            numeroDois = Integer.parseInt(edtSecondNumber.getText().toString());
+            numeroUm = Integer.parseInt(binding.edtFirstNumber.getText().toString());
+            numeroDois = Integer.parseInt(binding.edtSecondNumber.getText().toString());
 
             if (numeroDois != 0) {
                 int resultado = numeroUm / numeroDois;
-                txtResult.setText(String.valueOf(resultado));
+                binding.txtResult.setText(String.valueOf(resultado));
 
             } else {
                 Toast.makeText(this, "Não é possível dividir por 0.", Toast.LENGTH_SHORT).show();
@@ -88,11 +78,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         } else if (view.getId() == R.id.btn_times) {
 
-            numeroUm = Integer.parseInt(edtFirstNumber.getText().toString());
-            numeroDois = Integer.parseInt(edtSecondNumber.getText().toString());
+            numeroUm = Integer.parseInt(binding.edtFirstNumber.getText().toString());
+            numeroDois = Integer.parseInt(binding.edtSecondNumber.getText().toString());
 
             int resultado = numeroUm * numeroDois;
 
-            txtResult.setText(String.valueOf(resultado));
-        }}
+            binding.txtResult.setText(String.valueOf(resultado));
+        }
+    }
 }
